4424db5250745679e322f0364d368dbd58e54f49
THorvath1 <72626710+THorvath1@users.noreply.github.com>
2021-04-04 17:35:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import math
 import random
 import pygame
+import pygame_menu
 from pygame import mixer
-
-# Source code credit: attreyabhatt
-# https://github.com/attreyabhatt/Space-Invaders-Pygame 
 
 #initializes the game using pygame
 pygame.init()
@@ -12,11 +10,11 @@ pygame.init()
 #screen size
 screen = pygame.display.set_mode((800, 600))
 
-#background image
+#background image and menu bar
 background = pygame.image.load("./images/Very_Black_screen.png")
 
 #game audio
-mixer.music.load("background.wav")
+mixer.music.load("./audio/background.wav")
 mixer.music.play(-1)
 
 #name of the game and game icon
@@ -38,19 +36,24 @@ enemyX_change = []
 enemyY_change = []
 num_of_enemies = 6
 
+enemy_R = 1
+enemy_L = -1
+
 for i in range(num_of_enemies):
-    enemyImg.append(pygame.image.load("./images/spaceInvader.png"))
+    enemyImg.append(pygame.image.load("./images/enemy.png"))
     enemyX.append(random.randint(0, 736))
     enemyY.append(random.randint(50, 150))
-    enemyX_change.append(4)
-    enemyY_change.append(40)
+    enemyX_change.append(1)
+    enemyY_change.append(20)
 
-bulletImg = pygame.image.load("./images/bullet.png")
-bulletX = 0
-bulletY = 480
-bulletX_change = 0
-bulletY_change = 10
-bullet_state = "ready"
+
+#the laser
+laserImg = pygame.image.load("./images/laser.png")
+laserX = 0
+laserY = 480
+laserX_change = 0
+laserY_change = 10
+laser_state = "ready"
 
 #scoring
 
@@ -68,11 +71,19 @@ def show_score(x, y):
     score = font.render("Score : " + str(score_value), True, (255, 255, 255))
     screen.blit(score, (x, y))
 
-
 def game_over_text():
     over_text = over_font.render("GAME OVER", True, (255, 0, 0))
     screen.blit(over_text, (200, 250))
 
+def victory_text():
+    if victory == True:
+        over_text = font.render("Victory!", True, (255, 255, 255))
+        screen.blit(over_text, (355, 250))
+
+def play_again():
+    if game_over == True or victory == True:
+        yesNo = font.render("Press y to play again or n to quit.", True, (255, 255, 255))
+        screen.blit(yesNo, (355, 200))
 
 def player(x, y):
     screen.blit(playerImg, (x, y))
@@ -82,14 +93,14 @@ def enemy(x, y, i):
     screen.blit(enemyImg[i], (x, y))
 
 
-def fire_bullet(x, y):
-    global bullet_state
-    bullet_state = "fire"
-    screen.blit(bulletImg, (x + 16, y + 10))
+def fire_laser(x, y):
+    global laser_state
+    laser_state = "fire"
+    screen.blit(laserImg, (x + 16, y + 10))
 
 
 def isCollision(enemyX, enemyY, bulletX, bulletY):
-    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
+    distance = math.sqrt(math.pow(enemyX - laserX, 2) + (math.pow(enemyY - laserY, 2)))
     if distance < 27:
         return True
     else:
@@ -116,12 +127,12 @@ while running:
             if event.key == pygame.K_RIGHT:
                 playerX_change = 0.5
             if event.key == pygame.K_SPACE:
-                if bullet_state is "ready":
-                    bulletSound = mixer.Sound("laser.wav")
-                    bulletSound.play()
+                if  laser_state is "ready":
+                    laserSound = mixer.Sound("./audio/laser.wav")
+                    laserSound.play()
                     # Get the current x cordinate of the spaceship
-                    bulletX = playerX
-                    fire_bullet(bulletX, bulletY)
+                    laserX = playerX
+                    fire_laser(laserX, laserY)
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
@@ -133,6 +144,9 @@ while running:
         playerX = 0
     elif playerX >= 736:
         playerX = 736
+    
+   
+
 
     # Enemy Movement
     for i in range(num_of_enemies):
@@ -140,22 +154,25 @@ while running:
         # Game Over
         if enemyY[i] > 440:
             for j in range(num_of_enemies):
-                enemyY[j] = 2500
+                enemyY[i] = 2000
             game_over_text()
+            game_over = True
             break
 
+
         enemyX[i] += enemyX_change[i]
+
         if enemyX[i] <= 0: 
-            enemyX_change[i] = 1
+            enemyX_change[i] = enemy_R
             enemyY[i] += enemyY_change[i]
         elif enemyX[i] >= 736: 
-            enemyX_change[i] = -1
+            enemyX_change[i] = enemy_L
             enemyY[i] += enemyY_change[i]
 
         # Collision
-        collision = isCollision(enemyX[i], enemyY[i], bulletX, bulletY)
+        collision = isCollision(enemyX[i], enemyY[i], laserX, laserY)
         if collision:
-            explosionSound = mixer.Sound(".C:/Users/Teddy Horvath/Documents/CS232/p04-th150/explosion.wav")
+            explosionSound = mixer.Sound("./audio/explosion.wav")
             explosionSound.play() 
             bulletY = 480
             bullet_state = "ready"
@@ -165,14 +182,14 @@ while running:
 
         enemy(enemyX[i], enemyY[i], i)
 
-    # Bullet Movement
-    if bulletY <= 0:
-        bulletY = 480
-        bullet_state = "ready"
+    #laser movement
+    if laserY <= 0:
+        laserY = 480
+        laser_state = "ready"
 
-    if bullet_state is "fire":
-        fire_bullet(bulletX, bulletY)
-        bulletY -= bulletY_change
+    if laser_state is "fire":
+        fire_laser(laserX, laserY)
+        laserY -= laserY_change
 
     player(playerX, playerY)
     show_score(textX, testY)
